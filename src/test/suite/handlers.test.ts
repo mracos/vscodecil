a44602c839f2220d0f9821d5333cd194c128ca7b
Marcos Ferreira <marcosp4f@gmail.com>
2020-01-05 10:32:49 -0300

    Create test skeleton

---
+++ src/test/suite/handlers.test.ts
@@ -0,0 +1,11 @@
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+suite('handlers', () => {
+  describe('up', () => {
+    test('run pane commands defined in given itermocil layout file', () => { })
+    test('if no workspace is opened fails', () => { })
+    test('no layout file is given fails', () => { })
+    test('if given file does not exist fails', () => { })
+  })
+})
